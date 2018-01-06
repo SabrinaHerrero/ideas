5e73576773bcdd3d79b6bbb54ecad7c02d7f008c
SabrinaHerrero <sabrinaherrero123@gmail.com>
2018-01-06 15:29:04 -0600

    initial

---
+++ src/main/java/js/HelloWorldResource.java
@@ -0,0 +1,55 @@
+package js;
+
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+@Path("/b")
+public class HelloWorldResource {
+	
+	public HelloWorldResource() {
+		
+	}
+
+	@GET
+	@Path("/helloworld")
+	@Produces("text/html")
+	public String helloWorld() {
+		System.out.println("Inside helloworld");
+		return "Hello";
+	}
+	
+	@GET
+	@Path("/yo")
+	@Produces("text/html")
+	public String hello() {
+		return "Yo";
+	}
+	
+	@GET
+	@Path("/meetings")
+	@Produces("text/html")
+	public String helloAustin(){
+		String response = "<table><tr><td>Year</td><td>Number of Meetings</td></tr>";
+		for(int year = 2010; year <= 2017; year++) {
+			response += "<tr><td>"+ year + "</td><td>" + countMeetings("solum_team_meeting", year) + "</td></tr>";
+		}
+		response += "</table>";
+		return response;
+	}
+	
+	public int countMeetings(String proj, int year){
+		try {
+			Document html = Jsoup.connect("http://eavesdrop.openstack.org/meetings/"+proj+"/"+year+"/").timeout(5000).get();
+			return (html.select("td").select("a").size()-1)/4;
+		}catch(IOException e) {
+			return 0;
+		}
+	}
+
+}
