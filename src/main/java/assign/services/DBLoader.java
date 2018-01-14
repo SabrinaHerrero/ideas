c4fd0c92ff88a9bcc2c07f3b1ec833f9d9212d6b
jpalos <jpjessep@users.noreply.github.com>
2018-01-14 15:37:14 -0600

    maybe?

--- src/main/java/assign/services/DBLoader.java
+++ src/main/java/assign/services/DBLoader.java
@@ -1,6 +1,5 @@
 package assign.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -33,22 +32,20 @@ public class DBLoader {
 		logger.info("Inside loadData.");
 	}
 	
+
 	
 	public Long addDate(Date newDate) throws Exception {
 		Session session = sessionFactory.openSession();
 		Transaction tx = null;
 		Long dateId = null;
-		
-		//checks to make sure a valid input was given
 		if(emptyString(newDate.getTitle()) ||emptyString(newDate.getDateDescription())) {
 			return null;
 		}
-		
-		//saves new date object to table
 		try {
 			tx = session.beginTransaction();
 			session.save(newDate);
 		    dateId = newDate.getId();
+		    System.out.println("The new id is: "+ newDate.getId());
 		    tx.commit();
 		} catch (Exception e) {
 			if (tx != null) {
@@ -61,7 +58,7 @@ public class DBLoader {
 		}
 		return dateId;
 	}
-//checks to make sure the string is not entirely made up of white space
+
 	protected boolean emptyString(String str) {
 		if(str == null) {
 			return true;
@@ -100,6 +97,120 @@ public class DBLoader {
 //		
 //	}
 //	
+//	public Long addMeeting(Meeting meeting) throws Exception {
+//		Session session = sessionFactory.openSession();
+//		Transaction tx = null;
+//		Long meetingId = null;
+//		if(emptyString(meeting.getName())||invalidYear(meeting.getYear())) {
+//			return null;
+//		}
+//		try {
+//			tx = session.beginTransaction();
+//			session.save(meeting);
+//		    meetingId = meeting.getId();
+//		    tx.commit();
+//		} catch (Exception e) {
+//			if (tx != null) {
+//				tx.rollback();
+//				return null;  
+//			}
+//		}
+//		finally {
+//			session.close();
+//		}
+//		return meetingId;
+//	}
+//	
+//	public Long addMeetingAndProject(String title, String projectTitle) throws Exception {
+//		Session session = sessionFactory.openSession();
+//		Transaction tx = null;
+//		Long meetingId = null;
+//		try {
+//			tx = session.beginTransaction();
+//			Meeting newMeeting = new Meeting( );
+//			Date project = new Date(projectTitle, projectTitle);
+//			newMeeting.setProject(project);
+//			session.save(project);
+//			session.save(newMeeting);
+//		    meetingId = newMeeting.getId();
+//		    tx.commit();
+//		} catch (Exception e) {
+//			if (tx != null) {
+//				tx.rollback();
+//				throw e;
+//			}
+//		}
+//		finally {
+//			session.close();
+//		}
+//		return meetingId;
+//	}
+//	
+//	public Long addMeetingsToProject(List<String> meetings, String projectTitle) throws Exception {
+//		Session session = sessionFactory.openSession();
+//		Transaction tx = null;
+//		Long projectId = null;
+//		try {
+//			tx = session.beginTransaction();
+//			Date project = new Date(projectTitle, projectTitle);
+//			session.save(project);
+//			projectId = project.getId();
+//			for(String a : meetings) {
+//				Meeting newMeeting = new Meeting( );
+//				newMeeting.setProject(project);
+//				session.save(newMeeting);
+//			}
+//		    tx.commit();
+//		} catch (Exception e) {
+//			if (tx != null) {
+//				tx.rollback();
+//				throw e;
+//			}
+//		}
+//		finally {
+//			session.close();
+//		}
+//		return projectId;
+//	}
+//	
+//	public List<Meeting> getMeetingsForAProject(Long projectId) throws Exception {
+//		Session session = sessionFactory.openSession();		
+//		session.beginTransaction();
+//		String query = "from Meeting where project=" + projectId; // BAD PRACTICE
+//		List<Meeting> meetings = session.createQuery(query).list();
+//		session.close();
+//		return meetings;
+//	}
+//	
+//	public List<Object[]> getMeetingsForAProject(String projectName) throws Exception {
+//		Session session = sessionFactory.openSession();
+//		session.beginTransaction();
+//		String query = "from Meeting a join a.project c where c.projectName = :cname";		
+//				
+//		List<Object[]> meetings = session.createQuery(query).setParameter("cname", projectName).list();
+//		session.close();
+//		
+//		return meetings;
+//	}
+//	
+//	public Meeting getMeeting(Long id) throws Exception {
+//		Session session = sessionFactory.openSession();
+//		
+//		session.beginTransaction();
+//		
+//		Criteria criteria = session.createCriteria(Meeting.class).
+//        		add(Restrictions.eq("id", id));
+//		
+//		List<Meeting> meetings = criteria.list();
+//		
+//		
+//		if (meetings.size() > 0) {
+//			return meetings.get(0);			
+//		} else {
+//			return null;
+//		}
+//	}
+//	
 //	public Date getProject(String projectName) throws Exception {
 //		Session session = sessionFactory.openSession();
 //		
@@ -116,34 +227,53 @@ public class DBLoader {
 //			return null;
 //		}
 //	}
-	//returns a list of all Date objects in a table
-	public List<Date> getAllDates(){    
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		List<Date> list = new ArrayList<Date>();
-		list = session.createQuery("from Date").list();
-		return list;	    
-	}
-	
-	//returns a Date object that matches the given ID
-	public Date getDateById(Long id) throws Exception {
-		Session session = sessionFactory.openSession();
-		
-		session.beginTransaction();
-		
-		Criteria criteria = session.createCriteria(Date.class).
-        		add(Restrictions.eq("id", id));
-		
-		List<Date> dates = criteria.list();
-		
-		if (dates.size() > 0) {
-			return dates.get(0);	
-		} else {
-			return null;
-		}
-	}
+//	
+//	public Date getProject(Long id) throws Exception {
+//		Session session = sessionFactory.openSession();
+//		
+//		session.beginTransaction();
+//		
+//		Criteria criteria = session.createCriteria(Date.class).
+//        		add(Restrictions.eq("id", id));
+//		
+//		List<Date> projects = criteria.list();
+//		
+//		if (projects.size() > 0) {
+//			return projects.get(0);	
+//		} else {
+//			return null;
+//		}
+//	}
+//
+//	public void deleteMeeting(String title) throws Exception {
+//		
+//		Session session = sessionFactory.openSession();		
+//		session.beginTransaction();
+//		String query = "from Meeting a where a.title = :title";		
+//				
+//		Meeting a = (Meeting)session.createQuery(query).setParameter("title", title).list().get(0);
+//		
+//        session.delete(a);
+//
+//        session.getTransaction().commit();
+//        session.close();
+//	}
+//	
+//	public void deleteProject(Long id) throws Exception {
+//		
+//		Session session = sessionFactory.openSession();		
+//		session.beginTransaction();
+//		String query = "from Project c where c.id = :id";		
+//				
+//		Date c = (Date)session.createQuery(query).setParameter("id", id).list().get(0);
+//		
+//        session.delete(c);
+//
+//        session.getTransaction().commit();
+//        session.close();
+//	}
 
-	public void deleteDate(Long id) throws Exception {
+		public void deleteDate(Long id) throws Exception {
 		
 		Session session = sessionFactory.openSession();		
 		session.beginTransaction();
@@ -198,7 +328,6 @@ public class DBLoader {
 		private String newTableEntry(Date date, int index) {
 			StringBuilder sb = new StringBuilder();
 			String row = String.valueOf(index);
-			sb.append("<tr>");
 			sb.append("<th scope='row'>");
 			sb.append(row);
 			sb.append("</th>");
@@ -220,7 +349,6 @@ public class DBLoader {
 				sb.append(src);
 				sb.append("'> Album</td>");
 			}
-			sb.append("</tr>");
 			return sb.toString();
 		}
 
