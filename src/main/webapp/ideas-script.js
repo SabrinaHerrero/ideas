2d632037c5fbc2cdb563412c994ebf8e2ff3512a
Jesus Palos <jesus.rpalos@gmail.com>
2018-01-16 16:24:38 -0600

    POST form for new dates now working

--- src/main/webapp/ideas-script.js
+++ src/main/webapp/ideas-script.js
@@ -36,3 +36,63 @@
                 document.getElementById("incomplete-body").innerHTML = xmlhttp.responseText;
             });
          }
+
+
+        //  $.ajax({
+        //     url: ajaxurl,
+        //     data: "<test></test>", 
+        //     type: 'POST',
+        //     contentType: "text/xml",
+        //     dataType: "text",
+        //     success : parse,
+        //     error : function (xhr, ajaxOptions, thrownError){  
+        //         console.log(xhr.status);          
+        //         console.log(thrownError);
+        //     } 
+        // }); 
+        //basis for Post
+
+        // function $.ajax({
+        //     url: ajaxurl,
+        //     data: "<test></test>", 
+        //     type: 'POST',
+        //     contentType: "text/xml",
+        //     dataType: "text",
+        //     success : parse,
+        //     error : function (xhr, ajaxOptions, thrownError){  
+        //         console.log(xhr.status);          
+        //         console.log(thrownError);
+        //     } 
+        // }); 
+$(document).ready(function(){
+    $('#submitButton').on("click", function(event){
+     
+         let title = '' + $('#title').val();
+            let description = '' + $('#description').val();
+        let complete = '' + $('#completed').val();
+        let fecha = '' + $('#fecha').val();
+        let dataString = '<date><title>' + title + '</title><description>' + description + '</description><completed>' + complete + '</completed><fecha>' + fecha + '</fecha></date>';
+         $.ajax({ type: "POST",
+                        url: "ideas/dates",
+                        data: dataString,
+                        contentType: "application/xml",
+                        dataType: "xml",
+                        cache: false,
+                        error: function() { alert("No data found."); },
+                        success: function(xml) {
+//                            alert("it works");
+//                            alert($(xml).find("project")[0].attr("id"));
+                        }
+        });
+           event.preventDefault();
+    });
+});
+
+       // $("#txt_name").val()
+//$(document).ready(function(){
+//    $('#btnGetValue').click(function() {
+//        var selValue = $('input[name=rbnNumber]:checked').val(); 
+//        $('p').html('<br/>Selected Radio Button Value is : <b>' + selValue + '</b>');
+//    });
+//});
+        
